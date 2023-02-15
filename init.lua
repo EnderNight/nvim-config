63b702b0efdff08ece5b445e7e2ac2ed09990e20
EnderNight <ndxendernight@gmail.com>
2023-02-15 23:57:50 +0100

    Added default options in init.lua

---
+++ init.lua
@@ -0,0 +1,19 @@
+-- initial config file, meant to be changed afterwards
+
+vim.opt.showmatch = true
+
+vim.opt.ignorecase = true
+
+vim.opt.mouse = "a"
+
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+
+vim.opt.number = true
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.cursorline = true
+
+vim.opt.spell = true
