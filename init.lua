a0d88ad682026cd9bc53e26e66fe357ade8191f7
EnderNight <ndxendernight@gmail.com>
2023-02-17 00:49:09 +0100

    Added Lazy plugin manager and spell words

--- init.lua
+++ init.lua
@@ -5,3 +5,22 @@ require("default")
 if vim.g.neovide then
     require("neovide")
 end
+
+---- Lazy ----
+
+-- Installation
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then 
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", --latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Starting
+require("lazy").setup("plugins")
