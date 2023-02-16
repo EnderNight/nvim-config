cfb425bbc64189456eec6dda615e872a8c5cae74
EnderNight <ndxendernight@gmail.com>
2023-02-17 00:34:39 +0100

    Modified location of basic options

--- init.lua
+++ init.lua
@@ -1,24 +1,6 @@
 -- initial config file, meant to be changed afterwards
 
-vim.opt.showmatch = true
-
-vim.opt.ignorecase = true
-
-vim.opt.mouse = "a"
-
-vim.opt.tabstop = 4
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-
-vim.opt.number = true
-
-vim.opt.clipboard = "unnamedplus"
-
-vim.opt.cursorline = true
-
-vim.opt.spell = true
-
-vim.opt.guifont = { "FiraCode_Nerd_Font", ":h10" }
+require("default")
 
 if vim.g.neovide then
     require("neovide")
--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,1 +1,3 @@
-vim.opt.guifont = { "FiraCode_Nerd_Font", ":h10" }
+vim.opt.guifont = { "FiraCode_Nerd_Font", ":h10" } -- sets font
+vim.g.neovide_remember_window_size = true -- self-explanatory
+vim.g.neovide_cursor_vfx_mode = "ripple" -- cursor animation when changing mode
