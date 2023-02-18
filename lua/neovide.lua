3f737d272793facb50dff872da7ef9e9f4a8c2c5
EnderNight <ndxendernight@gmail.com>
2023-02-18 16:54:27 +0100

    Reset

--- lua/neovide.lua
+++
@@ -1,3 +0,0 @@
-vim.opt.guifont = { "FiraCode_Nerd_Font", ":h10" } -- sets font
-vim.g.neovide_remember_window_size = true -- self-explanatory
-vim.g.neovide_cursor_vfx_mode = "ripple" -- cursor animation when changing mode
