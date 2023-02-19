5c0dd2d7d36300574ea425fd8e93d6214ba44637
EnderNight <ndxendernight@gmail.com>
2023-02-19 20:04:15 +0100

    Added fuzzy finder

--- init.lua
+++ init.lua
@@ -2,18 +2,23 @@ vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({}, {})
+require("lazy").setup({
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	}
+}, {})
 
 
-
+require("mappings")
