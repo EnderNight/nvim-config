209ec7836d90387fd9b8da8d16c093165a0f136f
EnderNight <ndxendernight@gmail.com>
2023-02-19 21:13:28 +0100

    Added colorscheme

--- README.md
+++ README.md
@@ -18,3 +18,4 @@ git clone https://github.com/EnderNight/nvim-config ~/.config/nvim
 - Plugin Manager: [lazy.nvim](https://github.com/folke/lazy.nvim)
 - Fuzzy finder: [telescope.nvim](https://github.com/nvim-telescope/telescope.nvim)
 - Telescope fzf native: [telescope-fzf-native](https://github.com/nvim-telescope/telescope-fzf-native.nvim)
+- Colorscheme: [gruvbox-material](https://github.com/sainnhe/gruvbox-material)
--- init.lua
+++ init.lua
@@ -16,6 +16,34 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 
 	{
+		'sainnhe/gruvbox-material',
+		lazy = false,
+		priority = 1000,
+		config = function()
+			
+			if vim.fn.has('termguicolors') then
+				vim.opt.termguicolors = true
+			end
+
+			vim.g.gruvbox_material_background = 'hard'
+			vim.g.gruvbox_material_foreground = 'origingal'
+
+			vim.g.gruvbox_material_enable_bold = true
+			vim.g.gruvbox_material_enable_italic = true
+
+			vim.g.gruvbox_material_sign_column_background = 'grey'
+			vim.g.gruvbox_material_ui_contrast = 'high'
+
+			vim.g.gruvbox_material_diagnostic_text_highlight = 1
+			vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
+
+			vim.g.gruvbox_material_current_word = 'underline'
+
+			vim.cmd([[colorscheme gruvbox-material]])
+		end,
+	},
+
+	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.1',
 		dependencies = { 
 			'nvim-lua/plenary.nvim',
