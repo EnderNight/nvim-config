7c84e93ffe523eeb2fa9409960443dbd544a61be
EnderNight <ndxendernight@gmail.com>
2023-02-19 21:49:28 +0100

    Added default options

--- init.lua
+++ init.lua
@@ -147,3 +147,33 @@ cmp.setup(cmp_config)
 vim.diagnostic.config({
 	virtual_text = true,
 })
+
+
+
+-- Options --
+vim.o.showmatch = true
+
+vim.o.cursorline = true
+
+vim.opt.spell = true
+
+vim.o.hlsearch = false
+
+vim.wo.number = true
+vim.o.relativenumber = true
+
+vim.o.mouse = 'a'
+
+vim.o.clipboard = 'unnamedplus'
+
+vim.o.breakindent = true
+
+vim.o.undofile = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
