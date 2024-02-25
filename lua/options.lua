b04129874b0b1b8dca26060cc12b7554f8795d4d
Ender <ndxendernight@gmail.com>
2024-02-25 20:55:23 +0100

    Added basic options

---
+++ lua/options.lua
@@ -0,0 +1,39 @@
+--[[ Options ]]
+-- :help vim.o
+
+--- Line numbers
+vim.o.number = true
+vim.o.relativenumber = true
+
+--- Search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+--- Undo
+vim.o.undofile = true
+
+--- Tabs
+vim.o.tabstop = 4 -- Number of spaces per <Tab>
+vim.o.shiftwidth = 4 -- Number of spaces for each indent
+vim.o.expandtab = true -- Replace tabs with spaces
+vim.o.breakindent= true -- Preserve tabs for wrapped lines
+
+--- Mouse mode
+vim.o.mouse = 'a'
+
+--- Terminal colors
+vim.o.termguicolors = true
+
+--- Showmatch
+vim.o.showmatch = true -- Jump to matching brace
+
+--- Signcolumn
+vim.o.signcolumn = 'yes' -- Always display the signcolumn
+
+--- Splitting
+vim.o.splitright = true -- Always split to the right
+vim.o.splitbelow = true -- Always split to below
+
+--- Update time
+vim.o.updatetime = 250 -- Time before swap
+vim.o.timeoutlen = 300 -- Time before mapping
