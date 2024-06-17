729e3a655839fd0cf8cd30546a9d8cbce6140572
Ender <ndxendernight@gmail.com>
2024-06-17 22:25:30 +0200

    Removed action-hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -269,20 +269,6 @@ return {
 	},
 
 	{
-		"roobert/action-hints.nvim",
-
-		opts = {
-
-			template = {
-				definition = { text = " ⊛", color = "#add8e6" },
-				references = { text = " ↱%s", color = "#ff6666" },
-			},
-
-			use_virtual_text = true,
-		},
-	},
-
-	{
 		"smjonas/inc-rename.nvim",
 		config = true,
 	},
