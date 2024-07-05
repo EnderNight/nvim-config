62c5c4f86896518c9a89dbcf156717f21a806e4a
Ender <ndxendernight@gmail.com>
2024-07-05 20:26:30 +0200

    Added nvimtree mappings

--- lua/config/mappings/init.lua
+++ lua/config/mappings/init.lua
@@ -3,3 +3,5 @@ require("config.mappings.default")
 require("config.mappings.hop")
 
 require("config.mappings.terminal")
+
+require("config.mappings.tree")
---
+++ lua/config/mappings/tree.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", "<leader>tf", ":NvimTreeToggle<CR>", { desc = "Open the file tree" })
