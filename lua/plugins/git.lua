a7485f9f4ec0272c1e9b2bf675a1c33a9015cf6c
Ender <ndxendernight@gmail.com>
2024-07-05 16:02:58 +0200

    Added git related plugins

---
+++ lua/plugins/git.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        --- Great git functions for vim
+        "tpope/vim-fugitive",
+    },
+    {
+        --- Gitsigns in gutter
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            signs = {
+                add          = { text = '+' },
+                change       = { text = '~' },
+                delete       = { text = '_' },
+                topdelete    = { text = '‾' },
+                changedelete = { text = '~' },
+                untracked    = { text = '┆' },
+            },
+        },
+    }
+}
