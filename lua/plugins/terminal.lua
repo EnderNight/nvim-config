973fc52b79654bb81ca5401e39f4865bbcc825ef
Ender <ndxendernight@gmail.com>
2024-07-05 20:02:27 +0200

    Added terminal

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,10 @@
+return {
+    -- { "akinsho/toggleterm.nvim", version = "*", config = true },
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        opts = {
+            shell = "/usr/bin/fish",
+        },
+    },
+}
