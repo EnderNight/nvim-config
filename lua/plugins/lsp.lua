43ec77014593a8c3cd10248e8e30b21e19f513bb
Ender <ndxendernight@gmail.com>
2024-08-23 15:25:51 +0200

    Added nvim lint

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,30 @@ return {
         { "williamboman/mason.nvim", config = true },
         "williamboman/mason-lspconfig.nvim",
         "WhoIsSethDaniel/mason-tool-installer.nvim",
+        {
+            "mfussenegger/nvim-lint",
+            event = {
+                "BufReadPre",
+                "BufNewFile",
+            },
+            config = function()
+                local lint = require("lint")
+
+                lint.linters_by_ft = {
+                    cpp = { "cpplint" },
+                }
+
+                local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+                vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+                    group = lint_augroup,
+                    callback = function()
+                        lint.try_lint()
+                    end,
+                })
+            end,
+        },
+        "rshkarin/mason-nvim-lint",
 
         { "j-hui/fidget.nvim", opts = {} },
 
@@ -151,6 +175,8 @@ return {
                 end,
             },
         })
+
+        require("mason-nvim-lint").setup()
 
         require("lspconfig").ocamllsp.setup({})
     end,
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,23 +1,23 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    event = 'VimEnter',
-    branch = '0.1.x',
+    "nvim-telescope/telescope.nvim",
+    event = "VimEnter",
+    branch = "0.1.x",
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
+        "nvim-lua/plenary.nvim",
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
 
-        build = 'make',
+            build = "make",
 
-        --[[ Not necessary (who does not have make installed??)
+            --[[ Not necessary (who does not have make installed??)
         cond = function()
           return vim.fn.executable 'make' == 1
         end,
         ]]
-      },
-      { 'nvim-telescope/telescope-ui-select.nvim' },
+        },
+        { "nvim-telescope/telescope-ui-select.nvim" },
 
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+        { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
     },
     -- Useful commands
     --  - Insert mode: <c-/>
@@ -26,54 +26,57 @@ return {
     -- [[ Config ]]
     -- :help telescope and :help telescope.setup()
     config = function()
-
-      require('telescope').setup {
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
-        extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
-          },
-        },
-      }
-
-      pcall(require('telescope').load_extension, 'fzf')
-      pcall(require('telescope').load_extension, 'ui-select')
-
-      -- :help telescope.builtin
-      local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
-      vim.keymap.set('n', '<leader>/', function()
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
+        require("telescope").setup({
+            -- defaults = {
+            --   mappings = {
+            --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+            --   },
+            -- },
+            -- pickers = {}
+            extensions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown(),
+                },
+            },
         })
-      end, { desc = '[/] Fuzzily search in current buffer' })
 
-      --  :help telescope.builtin.live_grep()
-      vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
-        }
-      end, { desc = '[S]earch [/] in Open Files' })
+        pcall(require("telescope").load_extension, "fzf")
+        pcall(require("telescope").load_extension, "ui-select")
 
-      vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
+        -- :help telescope.builtin
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+        vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+        vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+        vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+        vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+        vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+        vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+        vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+        vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+        vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+
+        vim.keymap.set("n", "<leader>/", function()
+            builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+                winblend = 10,
+                previewer = false,
+            }))
+        end, { desc = "[/] Fuzzily search in current buffer" })
+
+        --  :help telescope.builtin.live_grep()
+        vim.keymap.set("n", "<leader>s/", function()
+            builtin.live_grep({
+                grep_open_files = true,
+                prompt_title = "Live Grep in Open Files",
+            })
+        end, { desc = "[S]earch [/] in Open Files" })
+
+        vim.keymap.set("n", "<leader>sn", function()
+            builtin.find_files({ cwd = vim.fn.stdpath("config") })
+        end, { desc = "[S]earch [N]eovim files" })
+
+        vim.keymap.set("n", "<leader>flutter", function()
+            require("telescope").extensions.flutter.commands()
+        end, { desc = "Open flutter commands" })
     end,
 }
