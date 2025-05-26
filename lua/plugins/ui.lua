0288d8de390bd60ea7b61704335d35b5b3ab7302
Ender <ndxendernight@gmail.com>
2025-05-27 00:15:30 +0200

    feat: add vim-sleuth

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,16 +1,17 @@
 local map = vim.keymap.set
 
 return {
-	{
-		'akinsho/bufferline.nvim',
-		version = "*",
-		dependencies = 'nvim-tree/nvim-web-devicons',
-		config = function(_, opts)
-			require("bufferline").setup(opts)
-			map("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous Buffer", })
-			map("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer", })
-			map("n", "<leader>bp", "<cmd>BufferLinePick<cr>", { desc = "Pick a Buffer", })
-			map("n", "<leader>bc", "<cmd>BufferLinePickClose<cr>", { desc = "Pick a Buffer to Close", })
-		end,
-	},
+    {
+        'akinsho/bufferline.nvim',
+        version = "*",
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function(_, opts)
+            require("bufferline").setup(opts)
+
+            map("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous Buffer", })
+            map("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer", })
+            map("n", "<leader>bp", "<cmd>BufferLinePick<cr>", { desc = "Pick a Buffer", })
+            map("n", "<leader>bc", "<cmd>BufferLinePickClose<cr>", { desc = "Pick a Buffer to Close", })
+        end,
+    },
 }
