ce5b2867f20f0fd4be590caf9eade5f666769e31
Ender <ndxendernight@gmail.com>
2025-05-26 03:29:52 +0200

    feat: add colorscheme

--- init.lua
+++ init.lua
@@ -17,55 +17,8 @@ vim.g.loaded_ruby_provider = 0
 
 
 
----[[ Options ]]---
-vim.opt.number = true
-vim.opt.relativenumber = true
+require("config.options")
 
-vim.opt.mouse = 'a'
+require("config.lazy")
 
-vim.opt.showmode = false
-
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
-
-vim.opt.breakindent = true
-
-vim.opt.undofile = true
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.signcolumn = 'yes'
-
-vim.opt.updatetime = 250
-
-vim.opt.timeoutlen = 300
-
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
-vim.opt.inccommand = 'split'
-
-vim.opt.cursorline = true
-
-vim.opt.scrolloff = 10
-
-vim.o.termguicolors = true
-
-
-
----[[ Commands ]]---
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup(
-    'kickstart-highlight-yank',
-    { clear = true }
-  ),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
+require("config.commands")
---
+++ lua/config/commands.lua
@@ -0,0 +1,11 @@
+---[[ Commands ]]---
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup(
+    'kickstart-highlight-yank',
+    { clear = true }
+  ),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
