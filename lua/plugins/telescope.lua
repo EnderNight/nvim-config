028076dfdf744d38da946842c35f8c4b6a5d60e2
Ender <ndxendernight@gmail.com>
2025-05-27 00:15:40 +0200

    feat: add telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+local map = vim.keymap.set
+
+return {
+    'nvim-telescope/telescope.nvim', 
+    branch = '0.1.x',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+    },
+
+    config = function(_, opts)
+        require("telescope").setup(opts)
+
+        local builtin = require("telescope.builtin")
+
+        map("n", "<leader>ff", builtin.find_files, { desc = "Telescope Find Files", })
+        map("n", "<leader>lg", builtin.live_grep, { desc = "Telescope Live Grep", })
+    end,
+}
