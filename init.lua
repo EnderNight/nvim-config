a48539c4e24f08b7e90ef833005ee717e65721e5
Ender <ndxendernight@gmail.com>
2025-05-26 03:06:36 +0200

    reset

--- init.lua
+++ init.lua
@@ -50,43 +50,6 @@ vim.opt.scrolloff = 10
 
 vim.o.termguicolors = true
 
----[[ Lazy.nvim configuration ]]---
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    '--branch=stable',
-    lazyrepo,
-    lazypath,
-  }
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-      { out, 'WarningMsg' },
-      { '\nPress any key to exit...' },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup {
-  spec = {
-    { import = 'editing' },
-    { import = 'git' },
-    { import = 'lsp' },
-    { import = 'navigation' },
-    { import = 'ui' },
-    { import = 'utilities' },
-  },
-  install = { colorscheme = { 'habamax' } },
-  checker = { enabled = true },
-}
-
 ---[[ Commands ]]---
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
@@ -98,5 +61,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
-
--- vim: ts=2 sts=2 sw=2 et
--- lua/ui/todo-comments.lua
+++
@@ -1,5 +0,0 @@
-return {
-  'folke/todo-comments.nvim',
-  event = 'VimEnter',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-}
